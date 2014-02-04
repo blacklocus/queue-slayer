e17a9bb4024df0f66c52dbc3fd7c8fa56b0d551f
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2014-02-04 17:34:30 +0000

    EC2MetadataClient is extraordinarily slow, even on EC2. Lookup the instance id once  (roughly once in the face of concurrency) and cache it.

--- qs-worker-aws/src/main/java/com/blacklocus/qs/worker/aws/AmazonEC2WorkerIdService.java
+++ qs-worker-aws/src/main/java/com/blacklocus/qs/worker/aws/AmazonEC2WorkerIdService.java
@@ -20,23 +20,53 @@ import com.blacklocus.qs.worker.QSWorkerIdService;
 import com.blacklocus.qs.worker.simple.HostNameQSWorkerIdService;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
+ * Identifies this worker by its Amazon EC2 instance id {@link EC2MetadataUtils#getInstanceId()}. Caches the value
+ * after first lookup forever. If the read fails, defers to a fallback QSWorkerIdService -- The fallback value is not
+ * cached and consulted every invocation.
+ *
  * @author Jason Dunkelberger (dirkraft)
  */
 public class AmazonEC2WorkerIdService implements QSWorkerIdService {
 
     public static final String PROP_SKIP_EC2_META = "com.blacklocus.qs.aws.disableEC2Meta";
 
-    private final QSWorkerIdService fallback = new HostNameQSWorkerIdService();
+
+    private final QSWorkerIdService fallback;
+
+    private final AtomicReference<String> ec2id = new AtomicReference<String>();
+
+
+    /**
+     * Defaults fallback to {@link HostNameQSWorkerIdService}
+     */
+    public AmazonEC2WorkerIdService() {
+        this(new HostNameQSWorkerIdService());
+    }
+
+    /**
+     * @param fallback in case the EC2 metadata lookup fails, most likely because this is not actually an EC2 instance.
+     *                 Disable EC2 checking and go straight to the fallback by setting {@link #PROP_SKIP_EC2_META} to
+     *                 <code>true</code>.
+     */
+    public AmazonEC2WorkerIdService(QSWorkerIdService fallback) {
+        this.fallback = fallback;
+    }
 
     @Override
     public String getWorkerId() {
-        String id = null;
+        String id = ec2id.get();
 
-        if (!Boolean.valueOf(System.getProperty(PROP_SKIP_EC2_META, "false"))) {
-            id = EC2MetadataUtils.getInstanceId();
+        if (null == id) {
+            if (!Boolean.valueOf(System.getProperty(PROP_SKIP_EC2_META, "false"))) {
+                id = EC2MetadataUtils.getInstanceId();
+                ec2id.set(id);
+            }
         }
 
+        // Maybe ec2 meta was disabled, maybe it failed. Whatever: fall back.
         if (StringUtils.isBlank(id)) {
             id = fallback.getWorkerId();
         }
